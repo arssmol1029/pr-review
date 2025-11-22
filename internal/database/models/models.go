8d4a3d51b063135d9678b87828bfdd33f7ac5720
arssmol1029@gmail.com <arssmol1029>
2025-11-22 12:04:55 +0300

    Change PRs AssignedReviewers field type && add Members field to Team

--- internal/database/models/models.go
+++ internal/database/models/models.go
@@ -3,23 +3,23 @@ package models
 import "time"
 
 type User struct {
-	ID       string `json:"user_id"`
+	ID       string `json:"user_id,omitempty"`
 	Username string `json:"username"`
 	IsActive bool   `json:"is_active"`
 	TeamName string `json:"team_name"`
 }
 
 type Team struct {
-	Name        string `json:"team_name"`
-	Description string `json:"description,omitempty"`
+	Name    string `json:"team_name"`
+	Members []User `json:"members,omitempty"`
 }
 
 type PullRequest struct {
-	ID                string     `json:"pull_request_id"`
+	ID                string     `json:"pull_request_id,omitempty"`
 	Name              string     `json:"pull_request_name"`
 	AuthorID          string     `json:"author_id"`
 	Status            string     `json:"status"` // OPEN, MERGED
-	AssignedReviewers []string   `json:"assigned_reviewers"`
+	AssignedReviewers []User     `json:"assigned_reviewers"`
 	CreatedAt         time.Time  `json:"created_at"`
 	MergedAt          *time.Time `json:"merged_at,omitempty"`
 }
