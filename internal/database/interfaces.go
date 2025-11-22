ee577634c3176b6c237d2de390be8fb103bfe577
arssmol1029@gmail.com <arssmol1029>
2025-11-22 13:31:15 +0300

    Delete unnecessary methods && add DB interface

--- internal/database/interfaces.go
+++ internal/database/interfaces.go
@@ -6,34 +6,35 @@ import (
 	"time"
 )
 
+type Database interface {
+	Init(ctx context.Context) error
+	Close() error
+	Ping(ctx context.Context) error
+}
+
 type UserRepository interface {
-	CreateUser(ctx context.Context, user *models.User) error
 	GetUserByID(ctx context.Context, id string) (*models.User, error)
-	GetUserByUsername(ctx context.Context, email string) (*models.User, error)
+	GetUserByUsername(ctx context.Context, username string) (*models.User, error)
 	SetUserActive(ctx context.Context, userID string, isActive bool) error
-	DeleteUser(ctx context.Context, id string) error
-	GetUsersByTeam(ctx context.Context, teamName string) ([]*models.User, error)
-	GetActiveUsersByTeam(ctx context.Context, teamName string) ([]*models.User, error)
+	GetPRsByReviewer(ctx context.Context, userID string) ([]*models.PullRequest, error)
+	UserExists(ctx context.Context, userID string) (bool, error)
 }
 
 type TeamRepository interface {
 	CreateTeam(ctx context.Context, team *models.Team) error
 	GetTeamByName(ctx context.Context, name string) (*models.Team, error)
-	DeleteTeam(ctx context.Context, name string) error
+	TeamExists(ctx context.Context, teamName string) (bool, error)
 }
 
 type PRRepository interface {
 	CreatePR(ctx context.Context, pr *models.PullRequest) error
 	GetPRByID(ctx context.Context, id string) (*models.PullRequest, error)
 	MergePR(ctx context.Context, prID string, mergedAt time.Time) error
-	AssignReviewer(ctx context.Context, prID, userID string) error
-	RemoveReviewer(ctx context.Context, prID, userID string) error
-	GetPRsByReviewer(ctx context.Context, userID string) ([]*models.PullRequest, error)
-	GetPRsByAuthor(ctx context.Context, authorID string) ([]*models.PullRequest, error)
+	ReassignReviewer(ctx context.Context, prID, userID string) error
 	PRExists(ctx context.Context, prID string) (bool, error)
 }
 
-type RepositoryCollection struct {
+type Repositories struct {
 	Users UserRepository
 	Teams TeamRepository
 	PRs   PRRepository
