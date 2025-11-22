3dc786e88d37d11e58aaf0c638e5a4a99df8971c
arssmol1029@gmail.com <arssmol1029>
2025-11-22 12:02:57 +0300

    Add database errors

--- internal/database/errors.go
+++ internal/database/errors.go
@@ -1,1 +1,18 @@
 package database
+
+import "errors"
+
+var (
+	ErrUserNotFound = errors.New("user not found")
+	ErrTeamNotFound = errors.New("team not found")
+	ErrPRNotFound   = errors.New("pull request not found")
+	ErrTeamExists   = errors.New("team already exists")
+	ErrPRExists     = errors.New("pull request already exists")
+	ErrPRMerged     = errors.New("pull request already merged")
+	ErrNotAssigned  = errors.New("reviewer is not assigned to this PR")
+	ErrNoCandidate  = errors.New("no active replacement candidate in team")
+)
+
+func WrapError(op string, err error) error {
+	return errors.New(op + ": " + err.Error())
+}
