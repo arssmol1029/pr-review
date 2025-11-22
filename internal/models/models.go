bb9cf744d5aca0e28d5b61ab0e52e6f4e0ec94b2
arssmol1029@gmail.com <arssmol1029>
2025-11-23 00:56:32 +0300

    Remove structured json tags

--- internal/models/models.go
+++ internal/models/models.go
@@ -3,31 +3,31 @@ package models
 import "time"
 
 type TeamMember struct {
-	UserID   string `json:"user_id,omitempty"`
-	Username string `json:"username"`
-	IsActive bool   `json:"is_active"`
+	UserID   string
+	Username string
+	IsActive bool
 }
 
 type User struct {
 	TeamMember
-	TeamName string `json:"team_name"`
+	TeamName string
 }
 
 type Team struct {
-	Name    string       `json:"team_name"`
-	Members []TeamMember `json:"members,omitempty"`
+	Name    string
+	Members []TeamMember
 }
 
 type PullRequestShort struct {
-	ID       string `json:"pull_request_id,omitempty"`
-	Name     string `json:"pull_request_name"`
-	AuthorID string `json:"author_id"`
-	Status   string `json:"status"` // OPEN, MERGED
+	ID       string
+	Name     string
+	AuthorID string
+	Status   string
 }
 
 type PullRequest struct {
 	PullRequestShort
-	AssignedReviewers []string   `json:"assigned_reviewers"`
-	CreatedAt         time.Time  `json:"created_at"`
-	MergedAt          *time.Time `json:"merged_at,omitempty"`
+	AssignedReviewers []string
+	CreatedAt         time.Time
+	MergedAt          *time.Time
 }
